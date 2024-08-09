0d085e817785617250d5f1cd15a1ab0b9aa4da04
sushantsharma-coco <cocosingh456@gmail.com>
2024-08-09 18:47:24 +0530

    msg

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const { json, urlencoded } = require("express");
 const userRouter = require("./routes/user.router");
 const notesRouter = require("./routes/notes.router");
 
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
