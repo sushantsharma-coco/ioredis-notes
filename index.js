fa72110b022633e445fcd14dd0b74d62d3ca67b4
sushantsharma-coco <cocosingh456@gmail.com>
2024-08-10 14:33:41 +0530

    sdaf

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
